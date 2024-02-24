93063635b7b49a60380c82277e67bea16492080e
Alexander Jaramillo <alexunjm@gmail.com>
2024-02-24 17:51:56 +0000

    struct encapsulation

--- designPatterns/01/factoryMethod/sea_logistics.go
+++ designPatterns/01/factoryMethod/sea_logistics.go
@@ -2,14 +2,14 @@ package factorymethod
 
 import "github.com/alexunjm/go-design-patterns/designPatterns/01/factoryMethod/transport"
 
-type SeaLogistics struct {
+type seaLogistics struct {
 }
 
 // CreateTransport implements Logistics.
-func (*SeaLogistics) CreateTransport() transport.Transport {
+func (*seaLogistics) CreateTransport() transport.Transport {
 	return transport.NewShip()
 }
 
 func NewSeaLogistics() Logistics {
-	return &SeaLogistics{}
+	return &seaLogistics{}
 }
